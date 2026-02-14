d6982af3097856281d03e38cf60ef6b3129dbaa8
Lohitha <23cse084@bnmit.in>
2026-02-14 11:10:29 +0530

    Added a JUnit test case

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        System.out.println("Running test case...");
+        assertTrue(true);
+    }
+}
